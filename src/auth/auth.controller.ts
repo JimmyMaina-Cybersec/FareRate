bfc0b59992a1cbed3167114fdc1601002c97866d
Namianya <wekesa350@gmail.com>
2023-06-25 13:39:00 +0300

    auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,10 +18,24 @@ export class AuthController {
     return this.authService.login(createAuthDto);
   }
 
-
   @UseGuards(JwtAuthGuard)
   @Post('logout')
   signOut(@CurrentUser('_id') userId: string) {
     return this.authService.signOut(userId);
   }
+
+  // @Post('local/signIn')
+  // signInLocal(@Body() createAuthDto: CreateAuthDto) {
+  //   return this.authService.login(createAuthDto);
+
+  // }
+
+  // @Post('local/signOutLocal')
+  // signOutLocal(@CurrentUser('_id') userId: string) {
+  //   return this.authService.signOut(userId);
+  // }
+
+  // @Post('local/refresh')
+  // refreshLocal() { }
+
 }
