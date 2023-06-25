d60d48880d15bfd721156629cd3909e5f350b7ce
Namianya <wekesa350@gmail.com>
2023-06-25 18:51:37 +0300

    removed trsaction rates

--- src/transaction-rate/dto/update-transaction-rate.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTransactionRateDto } from './create-transaction-rate.dto';
-
-export class UpdateTransactionRateDto extends PartialType(CreateTransactionRateDto) {}
