034ce93b418e4b0b8f5379a7e379c55a7794d50c
Namianya <wekesa350@gmail.com>
2023-06-28 13:59:08 +0300

    authenticate

--- src/currency-conversions/currency-conversions.controller.ts
+++ src/currency-conversions/currency-conversions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { CurrencyConversionsService } from './currency-conversions.service';
 import { CreateCurrencyConversionDto } from './dto/create-currency-conversion.dto';
 import { UpdateCurrencyConversionDto } from './dto/update-currency-conversion.dto';
@@ -6,11 +6,14 @@ import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { JwtPayload } from 'src/types/jwt-payload';
 import { query } from 'express';
 import PaginationQueryType from 'src/types/paginationQuery';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('currency-conversions')
 export class CurrencyConversionsController {
   constructor(private readonly currencyConversionsService: CurrencyConversionsService) { }
 
+
+  @UseGuards(JwtAuthGuard)
   @Post('create')
   create(@Body() createCurrencyConversionDto: CreateCurrencyConversionDto, @CurrentUser() user: JwtPayload) {
     return this.currencyConversionsService.create(createCurrencyConversionDto, user);
