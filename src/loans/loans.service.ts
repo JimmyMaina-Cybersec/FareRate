8bf6266d55a49b27db9c7482bd561afbf1f247de
Namianya <wekesa350@gmail.com>
2023-07-18 19:11:26 +0300

    claenaer loan payment

--- src/loans/loans.service.ts
+++ src/loans/loans.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateLoanDto } from './dto/create-loan.dto';
@@ -6,13 +6,14 @@ import { Loan } from './entities/loan.entity';
 import { CreateInstallmentDto } from './dto/installments.dto';
 import { Installment } from './entities/installment.entity';
 import { JwtPayload } from 'src/types/jwt-payload';
+import { UpdateLoanDto } from './dto/update-loan.dto';
 
 @Injectable()
 export class LoansService {
   constructor(
     @InjectModel(Loan.name) private loanModel: Model<Loan>,
     @InjectModel(Installment.name) private installmentModel: Model<Installment>,
-  ) {}
+  ) { }
 
   async create(createLoanDto: CreateLoanDto, user: JwtPayload) {
     const { amount, name, idNo, phoneNo } = createLoanDto;
@@ -24,7 +25,7 @@ export class LoansService {
       idNo,
       phoneNo,
       dateOfIssue,
-      user: user._id,
+      createdBy: user._id,
       shop: user.shop,
     });
 
@@ -46,8 +47,12 @@ export class LoansService {
     return await installment.save();
   }
 
-  async findAll() {
-    return await this.loanModel.find().exec();
+  async findAll(user: JwtPayload, filters: { shop?: String; agent?: string, page: number, resPerPage: number }) {
+    if (user.role === 'admin') {
+      return await this.loanModel.find().populate('createdBy').exec();
+    }
+    return await this.loanModel.find({ createdBy: user._id }).exec();
+
   }
 
   async findPendingLoansByIdNo(idNo: string) {
@@ -62,25 +67,47 @@ export class LoansService {
     return loans;
   }
 
-  async update(id: string, amount: number) {
-    const loan = await this.loanModel.findById(id).exec();
 
-    if (!loan) {
-      throw new NotFoundException(`Loan with ID ${id} not found`);
-    }
 
-    if (amount > loan.amount) {
-      throw new Error(
-        `Amount to be paid exceeds remaining debt ${loan.amount}`,
-      );
-    } else if (amount === loan.amount) {
-      loan.amount = 0;
-      await loan.save();
-      return 'Loan has been fully paid successfully';
-    } else {
-      loan.amount -= amount;
-      await loan.save();
-      return `Loan has been partially paid. Remaining debt is ${loan.amount}`;
+
+  async update(loanId: string, updateLoanDTO: UpdateLoanDto, user: JwtPayload) {
+
+    try {
+      const loan = await this.loanModel.findById(loanId).exec();
+
+      if (!loan) {
+        throw new NotFoundException(`Loan with ID ${loanId} not found`);
+      }
+
+      if (updateLoanDTO.amount > loan.balance) {
+        throw new Error(
+          `Amount to be paid exceeds remaining debt ${loan.balance}`,
+        );
+
+      }
+
+      const installment = await new this.installmentModel({
+        amountPaid: updateLoanDTO.amount,
+        user: user._id,
+        shop: user.shop,
+        loan: loanId,
+        balance: loan.balance - updateLoanDTO.amount,
+      }).save();
+
+      if (loan.balance === updateLoanDTO.amount) {
+        loan.status = 'paid';
+      } else {
+        loan.status = 'unpaid';
+      }
+      const updatedLoan = await this.loanModel.findByIdAndUpdate(loanId, {
+        $inc: { balance: -updateLoanDTO.amount },
+        $push: { installments: installment._id },
+        status: loan.status
+      }, { new: true });
+
+      return updatedLoan;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST)
     }
   }
 }
