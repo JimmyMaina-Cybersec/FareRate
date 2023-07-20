2151ed1b0ff9dd064feb8ddad2a3a6072897ce2c
Namianya <wekesa350@gmail.com>
2023-07-20 19:42:47 +0300

    resolve error

--- src/mobile-money/mobile-money.controller.ts
+++ src/mobile-money/mobile-money.controller.ts
@@ -1,34 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } from '@nestjs/common';
 import { MobileMoneyService } from './mobile-money.service';
 import { CreateMobileMoneyDto } from './dto/create-mobile-money.dto';
 import { UpdateMobileMoneyDto } from './dto/update-mobile-money.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { JwtPayload } from 'src/types/jwt-payload';
+import PaginationQueryType from 'src/types/paginationQuery';
+
+
+@UseGuards(JwtAuthGuard)
 
 @Controller('mobile-money')
 export class MobileMoneyController {
-  constructor(private readonly mobileMoneyService: MobileMoneyService) {}
+  constructor(private readonly mobileMoneyService: MobileMoneyService) { }
 
-  @Post()
-  create(@Body() createMobileMoneyDto: CreateMobileMoneyDto) {
-    return this.mobileMoneyService.create(createMobileMoneyDto);
+  @Post('add')
+  create(@Body() createMobileMoneyDto: CreateMobileMoneyDto, @CurrentUser() user: JwtPayload) {
+    return this.mobileMoneyService.create(createMobileMoneyDto, user);
   }
 
   @Get()
-  findAll() {
-    return this.mobileMoneyService.findAll();
+  findAll(
+    @Query() adminFilters: {
+      shop?: string;
+      createdBy?: string;
+      status?: string
+      agent?: string
+      _id?: string
+      provider?: string
+    },
+    @Query() agentFilters: {
+      status?: string
+      _id?: string
+      provider?: string
+    }, @Query() paginationQuery: PaginationQueryType, @CurrentUser() user: JwtPayload
+  ) {
+    return this.mobileMoneyService.findAll(
+      user,
+      adminFilters,
+      agentFilters,
+      paginationQuery
+    );
   }
 
-  @Get(':id')
+  @Get('provider/:id')
   findOne(@Param('id') id: string) {
-    return this.mobileMoneyService.findOne(+id);
+    return this.mobileMoneyService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMobileMoneyDto: UpdateMobileMoneyDto) {
-    return this.mobileMoneyService.update(+id, updateMobileMoneyDto);
+  @Patch('update/:id')
+  update(@Param('id') id: string, @Body() updateMobileMoneyDto: UpdateMobileMoneyDto, @CurrentUser() user: JwtPayload) {
+    return this.mobileMoneyService.update(id, updateMobileMoneyDto, user);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.mobileMoneyService.remove(+id);
+  @Delete('delete/:id')
+  remove(@Param('id') id: string, @CurrentUser() user: JwtPayload) {
+    return this.mobileMoneyService.remove(id, user);
   }
 }
