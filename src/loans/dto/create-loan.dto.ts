5dd7af21f11f53ae3050a27726641ab2dd1e429e
Jimmy-quiver <mainajimmy40@gmail.com>
2023-07-25 18:56:52 +0300

    changed totalLoan to loanAmount

--- src/loans/dto/create-loan.dto.ts
+++ src/loans/dto/create-loan.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class CreateLoanDto {
   @IsNotEmpty()
   @IsNumber()
-  totalLoan: number;
+  loanAmount: number;
 
   @IsNotEmpty()
   @IsString()
