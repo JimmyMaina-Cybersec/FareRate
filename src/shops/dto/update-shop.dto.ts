6102aac2cc50ec2056a82e01ea5e52f4310f7a1a
Jimmy-quiver <mainajimmy40@gmail.com>
2023-07-27 19:24:07 +0300

    added loan money to update shop dto

--- src/shops/dto/update-shop.dto.ts
+++ src/shops/dto/update-shop.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateShopDto } from './create-shop.dto';
+import { IsNumber } from 'class-validator';
 
-export class UpdateShopDto extends PartialType(CreateShopDto) {}
+export class UpdateShopDto extends PartialType(CreateShopDto) {
+  @IsNumber()
+  loanMoney: number;
+}
